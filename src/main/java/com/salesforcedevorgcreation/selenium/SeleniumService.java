6246f71287b633cf398869debfec56796b1bd45b
David Márquez Delgado <d.marquez.delgado.91@gmail.com>
2019-03-08 22:44:41 +0100

    Added selenium as dependency

---
+++ src/main/java/com/salesforcedevorgcreation/selenium/SeleniumService.java
@@ -0,0 +1,15 @@
+package com.salesforcedevorgcreation.selenium;
+
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+
+@Service
+public class SeleniumService {
+
+    @PostConstruct
+    public void testSelenium(){
+
+
+    }
+}
