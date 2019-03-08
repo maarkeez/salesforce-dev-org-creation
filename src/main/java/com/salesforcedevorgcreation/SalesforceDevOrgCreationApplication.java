d516141020a17d32c6f2124e4a7ec0489e89e31b
David Márquez Delgado <d.marquez.delgado.91@gmail.com>
2019-03-08 22:34:13 +0100

    Initial commit

---
+++ src/main/java/com/salesforcedevorgcreation/SalesforceDevOrgCreationApplication.java
@@ -0,0 +1,13 @@
+package com.salesforcedevorgcreation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesforceDevOrgCreationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesforceDevOrgCreationApplication.class, args);
+	}
+
+}
